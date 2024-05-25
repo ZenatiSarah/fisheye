820293390a960e495d6a9b4c0e706fe170829320
ZenatiSarah <62511328+ZenatiSarah@users.noreply.github.com>
2024-05-26 00:51:36 +0200

    afficher la bonne img sur ma lightbox

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -1,44 +1,37 @@
 export default class Lightbox {
     constructor(medias, datas) {
         this.medias = medias;
-        // const element = this.buildDOM(url);
-        //document.body.appendChild(element)
         this.addListener(medias, datas)
         console.log('dom construit !')
         this.divLightbox = document.querySelector('.section_lightbox');
-        //console.log(element)
         this.buildDOM()
     }
 
     addListener(medias, datas) {
         const links = Array.from(document.querySelectorAll('.medias'))
         links.forEach((link) => {
-            console.log(link)
             let index = link.dataset.index;
-            console.log(index)
             link.addEventListener('click', (e) => {
-
                 e.preventDefault();
-
-                const url = medias[index].image
+                let url = medias[index].image
                     ? `<img src="./assets/photographers/${datas[0].name.split(" ")[0]}/${medias[index].image}" alt="image de ${medias[index].image}" class="medias"/>`
                     : `<video height="240" controls> <source src="../../assets/photographers/${datas[0].name.split(" ")[0]} / ${medias[index].video}" type="video / mp4" class="medias"></video>`;
-                console.log(url)
-                this.showLightbox(index)
+                //this.showLightbox(index)
+                let lightboxContainer = document.querySelector('.lightbox__container');
+                lightboxContainer.innerHTML = url
+                console.log(lightboxContainer)
+                this.divLightbox.classList.add('visible');
+
             })
         })
-    }
 
-    showLightbox(indexImage) {
-        console.log(indexImage)
-        console.log(this.medias[indexImage])
-        this.showImage()
-        this.divLightbox.classList.add('visible');
     }
-    showImage() {
-        const lightboxContainer = document.querySelector('.lightbox__container')
-        lightboxContainer.innerHTML = `<img src="./assets/photographers/Ellie-Rose/Sport_Race_End.jpg" alt="image de Sport_Race_End.jpg" class="medias"/>`
-    }
+    /*
+        showLightbox(indexImage) {
+            console.log(indexImage)
+            console.log(this.medias[indexImage])
+            //this.showImage()
+        }*/
 
     buildDOM() {
         const dom = document.querySelector('.section_lightbox');
