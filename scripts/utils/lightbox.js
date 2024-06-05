b8651736a1b67be0e6ad92a8c634322c1d46c2dd
LouqHI <h.islamiya75@gmail.com>
2024-06-05 06:35:03 +0200

    lightbox fini

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -1,9 +1,11 @@
 export default class Lightbox {
     constructor(medias, datas) {
         this.medias = medias;
+        this.datas = datas
         this.addListener(medias, datas)
         console.log('dom construit !')
         this.divLightbox = document.querySelector('.section_lightbox');
+        this.index = 1;
         this.buildDOM()
     }
 
@@ -19,32 +21,25 @@ export default class Lightbox {
                 this.showIndex(index)
                 let lightboxContainer = document.querySelector('.lightbox__container');
                 lightboxContainer.innerHTML = url
-                console.log(url)
-                console.log("indx", index)
-                console.log(lightboxContainer)
                 this.divLightbox.classList.add('visible');
-                const nextSlide = document.querySelector('.lightbox__next');
-                nextSlide.addEventListener('click', () => {
-                    this.showIndex(index); //index is not defined
-                })
+
             })
         })
 
     }
 
     showIndex(indexImage) {
-        console.log(indexImage)
-        let nextIndex = indexImage++;
-        console.log(nextIndex)
+        this.index = indexImage;
+        console.log("Mon index actuel", this.index);
     }
 
     buildDOM() {
         const dom = document.querySelector('.section_lightbox');
         dom.classList.add('lightbox')
         dom.innerHTML = `
-        <button class="lightbox__close">Fermer</button>
-        <button class="lightbox__next">Suivant</button>
-        <button class="lightbox__prev">Précédent</button>
+        <button class="lightbox__close" aria-label="close">Fermer</button>
+        <button class="lightbox__next" aria-label="next">Suivant</button>
+        <button class="lightbox__prev" aria-label="previous">Précédent</button>
         <div class="lightbox__container"></div> `
 
         let boutonferme = dom.querySelector('.lightbox__close')
@@ -54,23 +49,53 @@ export default class Lightbox {
 
         const prevSlide = dom.querySelector('.lightbox__prev');
         prevSlide.addEventListener('click', () => {
-            this.previousSlide();
+            this.goPreviousSlide(this.medias, this.datas);
         });
-        /*const nextSlide = dom.querySelector('.lightbox__next');
+        const nextSlide = dom.querySelector('.lightbox__next');
         nextSlide.addEventListener('click', () => {
-            this.showIndex(index); //index is not defined
-        })*/
+            this.goNextSlide(this.medias, this.datas)
+        });
+        document.addEventListener('keydown', (e) => {
+            if (e.code === "ArrowRight") {
+                console.log(e.code)
+                this.goNextSlide(this.medias, this.datas);
+            }
+        });
+        document.addEventListener('keydown', (e) => {
+            if (e.code === "ArrowLeft") {
+                console.log(e.code)
+                this.goPreviousSlide(this.medias, this.datas);
+            }
+        });
+        document.addEventListener('keydown', (e) => {
+            console.log(e.code)
+            if (e.code === "Escape") {
+                this.hideLightbox();
+            }
+        });
         return dom;
     }
     hideLightbox() {
         this.divLightbox.classList.remove('visible');
     }
 
-    previousSlide() {
-        console.log('Slide précédente ')
+    goPreviousSlide(medias, datas) {
+        let lightboxContainer = document.querySelector('.lightbox__container');
+        const mediaLength = this.medias.length;
+        this.index == 0 ? this.index = mediaLength - 1 : this.index--;
+        let url = this.medias[this.index].image
+            ? `<img src="./assets/photographers/${datas[0].name.split(" ")[0]}/${medias[this.index].image}" alt="image de ${medias[this.index].image}" class="medias"/>`
+            : `<video height="240" controls> <source src="../../assets/photographers/${datas[0].name.split(" ")[0]} / ${medias[this.index].video}" type="video / mp4" class="medias"></video>`;
+        lightboxContainer.innerHTML = url
     }
-    nextSlide() {
-        console.log('Slide suivante ')
+    goNextSlide(medias, datas) {
+        const mediaLength = this.medias.length;
+        this.index >= mediaLength - 1 ? this.index = 0 : this.index++;
+        let lightboxContainer = document.querySelector('.lightbox__container');
+        let url = this.medias[this.index].image
+            ? `<img src="./assets/photographers/${datas[0].name.split(" ")[0]}/${medias[this.index].image}" alt="image de ${medias[this.index].image}" class="medias"/>`
+            : `<video height="240" controls> <source src="../../assets/photographers/${datas[0].name.split(" ")[0]} / ${medias[this.index].video}" type="video / mp4" class="medias"></video>`;
+        lightboxContainer.innerHTML = url
     }
 
 }
